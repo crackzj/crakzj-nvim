c488573043c7e44b81f8ccd6260fe79512514108
crackzj <crackzj@qq.com>
2023-03-01 13:19:01 +0800

    feat: telescope

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,5 +1,12 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
 --vscode
  
 -- 翻页
@@ -103,4 +110,3 @@ pluginKeys.maplsp = function(mapbuf)
 end
 
 return pluginKeys
-
